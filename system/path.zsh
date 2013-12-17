c57a36e0df3d0771d24bbbfbaaaf04f91542c2e0
soloman <soloman1124@gmail.com>
2013-12-17 22:53:55 +1000

    - setup and new work machine and clean up the dot files

--- system/path.zsh
+++ system/path.zsh
@@ -1,8 +1,7 @@
-PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$DOTFILES/bin:$PATH
+PATH=$PATH:$DOTFILES/bin
 
-#RUBY
-#PATH=$HOME/.rvm/gems/ruby-2.0.0-p0/bin:$HOME/.rvm/gems/ruby-2.0.0-p0@global/bin:$HOME/.rvm/rubies/ruby-2.0.0-p0/bin:$HOME/.rvm/bin:$PATH
-PATH=~/.rbenv/shims:$PATH
+#Homebrew
+PATH=/usr/local/sbin:$PATH
 
 #Coffee Script
 PATH=/usr/local/share/npm/bin:$PATH
