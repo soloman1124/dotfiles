b1d63db31fb7f2042ff944ec00704503ee16161b
soloman <soloman1124@gmail.com>
2013-12-18 18:03:13 +1000

    - enable git auto completion

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,13 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
+# use this might disable git auto completion
+#
+# hub_path=$(which hub)
+# if (( $+commands[hub] ))
+# then
+#   alias git=$hub_path
+# fi
+#
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
--- git/completion.zsh
+++ git/completion.zsh
@@ -1,9 +1,12 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
-
-if test -f $completion
-then
-  source $completion
-fi
+#
+# This is disabled as git-completion.zsh is preferred for zsh env
+#
+# completion=/usr/local/etc/bash_completion.d/git-completion.bash
+#
+# if test -f $completion
+# then
+#   source $completion
+# fi
