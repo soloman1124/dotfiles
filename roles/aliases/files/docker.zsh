7b5be6e0c028f76e7050c7a1dc70aeb7fac95358
Soloman Weng <soloman1124@gmail.com>
2017-01-31 12:30:45 +1000

    docker fixes

--- roles/aliases/files/docker.zsh
+++ roles/aliases/files/docker.zsh
@@ -2,12 +2,21 @@ _docker_compose_bash() {
   docker-compose run --rm $1 /bin/bash
 }
 
+_docker_devtools() {
+  if [ $# -eq 1 ]; then
+    docker run --rm -ti -p $1:$1 -v $(pwd):/usr/src/app -w /usr/src/app soloman1124/devtools /sbin/my_init -- bash -l
+  else
+    docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app soloman1124/devtools /sbin/my_init -- bash -l
+  fi
+}
+
 alias docker-ps-cleanup='docker ps -aq -f status=exited | xargs docker rm'
-alias docker-cleanup='docker images -fq dangling=true | xargs docker rmi'
+alias docker-cleanup='docker images -aq -f dangling=true | xargs docker rmi'
 alias docker-elixir='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app trenpixster/elixir:latest /bin/bash'
 alias docker-ruby='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app ruby:2.1.5 /bin/bash'
 alias docker-node='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app node:0.10 /bin/bash'
 alias docker-go='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app --entrypoint /bin/bash soloman1124/go-tools:latest'
 alias docker-gore='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app soloman1124/go-tools:latest'
+alias docker-devtools=_docker_devtools
 
 alias dc-bash=_docker_compose_bash
