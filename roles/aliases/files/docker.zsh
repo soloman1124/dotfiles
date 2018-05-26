1dd4596647fbe43f59cc6f18da671902c739eb09
Soloman Weng <soloman1124@gmail.com>
2018-05-26 23:41:09 +1000

    Progress checkin

--- roles/aliases/files/docker.zsh
+++ roles/aliases/files/docker.zsh
@@ -3,7 +3,7 @@ _docker_compose_bash() {
 }
 
 alias docker-ps-cleanup='docker ps -aq -f status=exited | xargs docker rm'
-alias docker-cleanup='docker images -fq dangling=true | xargs docker rmi'
+alias docker-cleanup='docker system prune'
 alias docker-elixir='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app trenpixster/elixir:latest /bin/bash'
 alias docker-ruby='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app ruby:2.1.5 /bin/bash'
 alias docker-node='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app node:0.10 /bin/bash'
