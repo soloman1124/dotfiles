c6f02a31c6d33d2634aeb68ad3a3dfc7fc55ed28
Soloman Weng <soloman1124@gmail.com>
2018-06-18 11:53:16 +1000

    Add docker toolbox

--- roles/aliases/files/docker.zsh
+++ roles/aliases/files/docker.zsh
@@ -2,6 +2,14 @@ _docker_compose_bash() {
   docker-compose run --rm $1 /bin/bash
 }
 
+_docker_devtools() {
+  if [ $# -eq 1 ]; then
+    docker run --rm -ti -p $1:$1 -v $(pwd):/usr/src/app -w /usr/src/app soloman1124/devtools /sbin/my_init -- bash -l
+  else
+    docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app soloman1124/devtools /sbin/my_init -- bash -l
+  fi
+}
+
 alias docker-ps-cleanup='docker ps -aq -f status=exited | xargs docker rm'
 alias docker-cleanup='docker system prune'
 alias docker-elixir='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app trenpixster/elixir:latest /bin/bash'
@@ -11,4 +19,6 @@ alias docker-go='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app --en
 alias docker-gore='docker run --rm -ti -v $(pwd):/usr/src/app -w /usr/src/app soloman1124/go-tools:latest'
 alias docker-jupyter='docker run --rm -p 8888:8888 -v $(pwd):/home/jovyan/work jupyter/tensorflow-notebook:latest'
 
+
+alias docker-devtools=_docker_devtools
 alias dc-bash=_docker_compose_bash
