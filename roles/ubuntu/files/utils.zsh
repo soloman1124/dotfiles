49c2e6081d970980aaca6f07b1216ab9bb353169
Soloman Weng <soloman1124@gmail.com>
2020-10-31 01:24:03 +1000

    Add support to free wsl resources

--- roles/ubuntu/files/utils.zsh
+++ roles/ubuntu/files/utils.zsh
@@ -2,4 +2,11 @@ httpless() {
   http --pretty=all --print=hb "$@" | less -R;
 }
 
+# https://devblogs.microsoft.com/commandline/memory-reclaim-in-the-windows-subsystem-for-linux-2/
+_wsl_free() {
+  sudo sh -c "/bin/echo 1 > /proc/sys/vm/compact_memory"
+}
+
 alias open=explorer.exe
+
+alias wsl_free=_wsl_free
