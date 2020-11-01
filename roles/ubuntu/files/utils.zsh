eeaf6b3da9544fa5a14e37f844e1a3d6cc7d4559
Soloman Weng <soloman1124@gmail.com>
2020-11-02 09:29:47 +1000

    Add support to fix zsh history

--- roles/ubuntu/files/utils.zsh
+++ roles/ubuntu/files/utils.zsh
@@ -7,6 +7,15 @@ _wsl_free() {
   sudo sh -c "/bin/echo 1 > /proc/sys/vm/compact_memory"
 }
 
+_fix_zsh_history() {
+  mv ~/.zsh_history ~/.zsh_history_bad
+  strings ~/.zsh_history_bad > ~/.zsh_history
+  fc -R ~/.zsh_history
+  rm ~/.zsh_history_bad
+}
+
 alias open=explorer.exe
 
 alias wsl_free=_wsl_free
+
+alias fix_zsh_history=_fix_zsh_history
